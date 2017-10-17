edee0316e5143fd8ccfc5b8c07bc1617be841c5a
Erica-Brock <Eady.Brock@gmail.com>
2017-10-17 11:27:01 -0500

    added thumbnails and link

--- list.js
+++ list.js
@@ -5,13 +5,24 @@ $.ajax({
     let post= sucess.data.children;
     console.log(post);
     for(i=0; i<post.length; i++){
-        postContainer=document.createElement("div");
-        link=document.createElement("a");
-        title=post[i].data.title;
-        link.href=post[i].data.url;
+        let postContainer=document.createElement("div");
+        let link=document.createElement("a");
+        let previewImg=document.createElement("img");
+        let title=post[i].data.title;
+        let thumbnail=post[i].data.thumbnail;
+        previewImg.className ="thumbnail"
+        if(thumbnail=="self"){
+            previewImg.src="images/thumbs-up.png";
+        }else{
+        previewImg.src=thumbnail;}
+       // link.href=post[i].data.url;
+       link.href="single.html/?url=";
+       console.log(link.this);
         link.innerHTML=title;
-        document.body.appendChild(postContainer);
         postContainer.appendChild(link);
+        postContainer.appendChild(previewImg);
+        document.body.appendChild(postContainer);
+        
     }
 
 })
---
+++ single.js
@@ -0,0 +1,8 @@
+// $document.ready(function(){
+//     let div=document.createElement("div");
+//     let title;
+//     let thumbnail;
+//     div.appendChild(title);
+//     div.appendChild(thumbnail);
+//     document.body.appendChild(div);
+// })
